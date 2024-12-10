b3c88ceadfe9f077e082e3c46243954895898f5c
Marek Schir <schir2@gmail.com>
2024-12-09 22:59:09 -0500

    removed Config from all models, viewsets and serializers

--- core/urls.py
+++ core/urls.py
@@ -5,47 +5,47 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from main.viewsets import (
-    BrokerageInvestmentConfigViewSet,
+    BrokerageInvestmentViewSet,
     BrokerageInvestmentTemplateViewSet,
-    CashConfigViewSet,
+    CashViewSet,
     CashTemplateViewSet,
-    DebtConfigViewSet,
+    DebtViewSet,
     DebtTemplateViewSet,
-    ExpenseConfigViewSet,
+    ExpenseViewSet,
     ExpenseTemplateViewSet,
-    IncomeConfigViewSet,
+    IncomeViewSet,
     IncomeTemplateViewSet,
-    IraInvestmentConfigViewSet,
+    IraInvestmentViewSet,
     IraInvestmentTemplateViewSet,
-    TaxDeferredInvestmentConfigViewSet,
+    TaxDeferredInvestmentViewSet,
     TaxDeferredInvestmentTemplateViewSet,
-    PlanConfigViewSet,
+    PlanViewSet,
     PlanTemplateViewSet,
 )
 
 router = DefaultRouter()
-router.register(r'brokerage-investment-configs', BrokerageInvestmentConfigViewSet)
+router.register(r'brokerage-investments', BrokerageInvestmentViewSet)
 router.register(r'brokerage-investment-templates', BrokerageInvestmentTemplateViewSet)
-router.register(r'cash-configs', CashConfigViewSet)
+router.register(r'cashes', CashViewSet)
 router.register(r'cash-templates', CashTemplateViewSet)
-router.register(r'debt-configs', DebtConfigViewSet)
+router.register(r'debts', DebtViewSet)
 router.register(r'debt-templates', DebtTemplateViewSet)
-router.register(r'expense-configs', ExpenseConfigViewSet)
+router.register(r'expenses', ExpenseViewSet)
 router.register(r'expense-templates', ExpenseTemplateViewSet)
-router.register(r'income-configs', IncomeConfigViewSet)
+router.register(r'incomes', IncomeViewSet)
 router.register(r'income-templates', IncomeTemplateViewSet)
-router.register(r'ira-investment-configs', IraInvestmentConfigViewSet)
+router.register(r'ira-investments', IraInvestmentViewSet)
 router.register(r'ira-investment-templates', IraInvestmentTemplateViewSet)
-router.register(r'tax-deferred-investment-configs', TaxDeferredInvestmentConfigViewSet)
+router.register(r'tax-deferred-investments', TaxDeferredInvestmentViewSet)
 router.register(r'tax-deferred-investment-templates', TaxDeferredInvestmentTemplateViewSet)
-router.register(r'plan-configs', PlanConfigViewSet)
+router.register(r'plans', PlanViewSet)
 router.register(r'plan-templates', PlanTemplateViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
     path('users/', include('django.contrib.auth.urls', )),
-    path('', include(router.urls))
+    path('api/', include(router.urls))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
