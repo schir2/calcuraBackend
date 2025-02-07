24d50e32c1952c8b52adaf77fcdcb72b23fd7927
Marek Schir <schir2@gmail.com>
2025-02-06 19:42:39 -0500

    removed unused comments and imports

--- main/signals.py
+++ main/signals.py
@@ -1,10 +1,9 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.db import models
 
 from main.models import Income, TaxDeferredInvestment, Debt, Command, Expense, BrokerageInvestment, IraInvestment, \
-    RothIraInvestment, CashReserve, CommandSequence, CommandSequenceCommand
+    RothIraInvestment, CashReserve
 
 
 @receiver(post_save, sender=Debt)
@@ -15,9 +14,7 @@ from main.models import Income, TaxDeferredInvestment, Debt, Command, Expense, B
 @receiver(post_save, sender=IraInvestment)
 @receiver(post_save, sender=RothIraInvestment)
 @receiver(post_save, sender=CashReserve)
-
 def create_command_for_related_models(sender, instance, created, **kwargs):
-    """ Creates a Command when a related model is created and adds it to the Plan’s CommandSequence """
     if created:
         content_type = ContentType.objects.get_for_model(instance)
         command = Command.objects.create(
@@ -25,6 +22,7 @@ def create_command_for_related_models(sender, instance, created, **kwargs):
             object_id=instance.id,
             action="create"
         )
+
 
 @receiver(post_delete, sender=Debt)
 @receiver(post_delete, sender=Expense)
@@ -35,6 +33,5 @@ def create_command_for_related_models(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=RothIraInvestment)
 @receiver(post_delete, sender=CashReserve)
 def delete_command_for_debt(sender, instance, **kwargs):
-    """ Deletes associated Commands when a Debt is deleted """
     content_type = ContentType.objects.get_for_model(instance)
     Command.objects.filter(content_type=content_type, object_id=instance.id).delete()
