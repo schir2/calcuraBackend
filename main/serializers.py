714742fcec0ddd3ac1c313a88d764d0fba461959
Marek Schir <schir2@msn.com>
2025-02-15 15:19:00 -0500

    added create and update user with hashed passwords

--- main/serializers.py
+++ main/serializers.py
@@ -12,6 +12,22 @@ class UserSerializer(serializers.ModelSerializer):
             'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser',
             'groups', 'password')
 
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class IncomeOrIdField(serializers.BaseSerializer):
     def to_internal_value(self, data):
