43353ae3fe6752536d356f312220d1c9b23649c9
Marek Schir <schir2@msn.com>
2025-02-28 01:10:34 -0500

    added nested router

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter, DefaultRouter
 
 from main.viewsets import (
-    BrokerageInvestmentViewSet,
-    BrokerageInvestmentTemplateViewSet,
+    BrokerageViewSet,
+    BrokerageTemplateViewSet,
     CashReserveViewSet,
     CashReserveTemplateViewSet,
     DebtViewSet,
@@ -15,43 +15,55 @@ from main.viewsets import (
     ExpenseTemplateViewSet,
     IncomeViewSet,
     IncomeTemplateViewSet,
-    IraInvestmentViewSet,
-    IraInvestmentTemplateViewSet,
-    TaxDeferredInvestmentViewSet,
-    TaxDeferredInvestmentTemplateViewSet,
+    IraViewSet,
+    IraTemplateViewSet,
+    TaxDeferredViewSet,
+    TaxDeferredTemplateViewSet,
     PlanViewSet,
-    PlanTemplateViewSet, RothIraInvestmentViewSet, RothIraInvestmentTemplateViewSet, CommandSequenceCommandViewSet,
+    PlanTemplateViewSet, RothIraViewSet, RothIraTemplateViewSet, CommandSequenceCommandViewSet,
     CommandSequenceViewSet, CommandViewSet,
 )
 from users.views import login_view, logout_view, get_csrf_token, register_view, verify_view, email_exists_view
 from users.viewsets import UserViewSet
 
 router = DefaultRouter()
-router.register('brokerage-investments', BrokerageInvestmentViewSet, basename='brokerage-investment')
-router.register('brokerage-investment-templates', BrokerageInvestmentTemplateViewSet,
-                basename='brokerage-investment-template')
+router.register('brokerages', BrokerageViewSet, basename='brokerage')
 router.register('cash-reserves', CashReserveViewSet, basename='cash-reserve')
-router.register('cash-reserve-templates', CashReserveTemplateViewSet, basename='cash-reserve-template')
 router.register('debts', DebtViewSet, basename='debt')
-router.register('debt-templates', DebtTemplateViewSet, basename='debt-template')
 router.register('expenses', ExpenseViewSet, basename='expense')
-router.register('expense-templates', ExpenseTemplateViewSet, basename='expense-template')
 router.register('incomes', IncomeViewSet, basename='income')
-router.register('income-templates', IncomeTemplateViewSet, basename='income-template')
-router.register('ira-investments', IraInvestmentViewSet, basename='ira-investment')
-router.register('ira-investment-templates', IraInvestmentTemplateViewSet, basename='ira-investment-template')
-router.register('roth-ira-investments', RothIraInvestmentViewSet, basename='roth-ira-investment')
-router.register('roth-ira-investment-templates', RothIraInvestmentTemplateViewSet,
-                basename='roth-ira-investment-template')
-router.register('tax-deferred-investments', TaxDeferredInvestmentViewSet, basename='tax-deferred-investment')
-router.register('tax-deferred-investment-templates', TaxDeferredInvestmentTemplateViewSet,
-                basename='tax-deferred-investment-template')
-router.register('plans', PlanViewSet, basename='plan')
-router.register('plan-templates', PlanTemplateViewSet, basename='plan-template')
+router.register('iras', IraViewSet, basename='ira')
+router.register('roth-iras', RothIraViewSet, basename='roth-ira')
+router.register('tax-deferreds', TaxDeferredViewSet, basename='tax-deferred')
 router.register('commands', CommandViewSet, basename='command')
 router.register('command-sequences', CommandSequenceViewSet, basename='command-sequence')
 router.register('command-sequence-commands', CommandSequenceCommandViewSet, basename='command-sequence-command')
+router.register('plans', PlanViewSet, basename='plan')
 router.register('users', UserViewSet, basename='user')
+
+plan_router = NestedDefaultRouter(router, r'plans', lookup='plan')
+plan_router.register(r'brokerages', BrokerageViewSet, basename='plan-brokerage')
+plan_router.register(r'cash-reserves', CashReserveViewSet, basename='plan-cash-reserve')
+plan_router.register(r'debts', DebtViewSet, basename='plan-debt')
+plan_router.register(r'expenses', ExpenseViewSet, basename='plan-expense')
+plan_router.register(r'incomes', IncomeViewSet, basename='plan-income')
+plan_router.register(r'iras', IraViewSet, basename='plan-ira')
+plan_router.register(r'roth-iras', RothIraViewSet, basename='plan-roth-ira')
+plan_router.register(r'tax-deferreds', TaxDeferredViewSet, basename='plan-tax-deferred')
+
+
+router.register('brokerage-templates', BrokerageTemplateViewSet,
+                basename='brokerage-template')
+router.register('cash-reserve-templates', CashReserveTemplateViewSet, basename='cash-reserve-template')
+router.register('debt-templates', DebtTemplateViewSet, basename='debt-template')
+router.register('expense-templates', ExpenseTemplateViewSet, basename='expense-template')
+router.register('income-templates', IncomeTemplateViewSet, basename='income-template')
+router.register('ira-templates', IraTemplateViewSet, basename='ira-template')
+router.register('roth-ira-templates', RothIraTemplateViewSet,
+                basename='roth-ira-template')
+router.register('tax-deferred-templates', TaxDeferredTemplateViewSet,
+                basename='tax-deferred-template')
+router.register('plan-templates', PlanTemplateViewSet, basename='plan-template')
 
 urlpatterns = [
 
@@ -59,6 +71,7 @@ urlpatterns = [
     path('api/', include('rest_framework.urls')),
     path('users/', include('django.contrib.auth.urls', )),
     path('api/', include(router.urls)),
+    path('api/', include(plan_router.urls)),
     path("api/auth/login/", login_view, name="login"),
     path("api/auth/logout/", logout_view, name="logout"),
     path("api/auth/register/", register_view, name="register"),
