4d53d03f190f1e13c8d648eba6e05bcc0ee74d7a
Marek Schir <schir2@msn.com>
2025-02-28 15:40:36 -0500

    fixed related managers

--- main/viewsets.py
+++ main/viewsets.py
@@ -223,45 +223,6 @@ class PlanViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(creator=self.request.user)
 
-    @action(detail=True, methods=['post'])
-    def manage_related_model(self, request, pk=None):
-        plan = self.get_object()
-
-        serializer = ManageRelatedModelSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
-
-        related_model = validated_data['related_model']
-        related_pk = validated_data['related_id']
-        action = validated_data['action']
-
-        if related_model not in get_many_to_many_fields(plan):
-            raise Http404(f'{related_model} is not a valid related model for Plan')
-
-        related_manager = getattr(plan, related_model)
-
-        RelatedModel = related_manager.model
-
-        try:
-            related_instance = RelatedModel.objects.get(id=related_pk)
-        except RelatedModel.DoesNotExist:
-            raise Http404(f'{RelatedModel.__name__} with ID {related_pk} does not exist.')
-
-        with transaction.atomic():
-            if action == 'add':
-                related_manager.add(related_instance)
-                message = f"Successfully added {RelatedModel.__name__} with ID {related_pk} to {related_model}."
-            elif action == 'remove':
-                related_manager.remove(related_instance)
-                message = f"Successfully removed {RelatedModel.__name__} with ID {related_pk} from {related_model}."
-
-        plan.refresh_from_db()
-        serialized_plan = self.get_serializer(plan)
-
-        return Response(
-            serialized_plan.data, status=200
-        )
-
 
 class PlanTemplateViewSet(viewsets.ModelViewSet):
     queryset = PlanTemplate.objects.all()
